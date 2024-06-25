29ae8890ffcf530b0587e71a587c1136a8b214bd
sebastian <sebastian.guerra17@inacapmail.cl>
2024-06-24 22:26:44 -0400

    funcion logica de imc modificada en .js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,46 @@ function procesarFormulario(evento) {
     //alert('IOOOOOOO')
     //obtener el usuario desde input txtUsuario
     const usuario = document.getElementById("username").value
-    //obtener la contraseña desde input txtPassword
+    //obtener el peso desde input weight
     const peso = document.getElementById("weight").value
-    //obtener "recordarme" desde input chkRecordar
+    //obtener la altura desde input height
     const altura = document.getElementById("height").value
+    //obtener el div de la id "result"
+    const resultado = document.getElementById("result")
     console.log(usuario, peso, altura)
+    let imc = peso/altura**2
+
+    if (imc < 18.5) {
+        resultado.innerHTML = `
+        <div class="col-12">
+            <div class="alert alert-primary" id="result" role="alert" >
+                Delgadez o bajo peso
+            </div>
+        </div>
+            `
+    } else if (imc >= 18.5 && imc < 24.9) {
+        resultado.innerHTML = `
+        <div class="col-12">
+            <div class="alert alert-primary" id="result" role="alert" >
+                normal
+            </div>
+        </div>
+        `
+    } else if (imc >= 25.0 && imc < 29.9) {
+        resultado.innerHTML = `
+            <div class="col-12">
+                <div class="alert alert-primary" id="result" role="alert" >
+                    sobrepeso
+                </div>
+            </div>
+            `
+    } else {
+        resultado.innerHTML = `
+            <div class="col-12">
+                <div class="alert alert-primary" id="result" role="alert" >
+                    obesidad
+                </div>
+            </div>
+            `
+    }
 }
